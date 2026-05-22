e8b97efca2b2a69c4a463049863d4637595ef382
zhongqin <snkzhong@163.com>
2026-05-22 13:37:37 +0800

    separate

--- frontend/admin/src/lib/types.ts
+++
@@ -1,4 +0,0 @@
-import type { PostResponse, CommentResponse } from "@raisfast/sdk";
-
-export type Post = Omit<PostResponse, "id"> & { id: string };
-export type Comment = Omit<CommentResponse, "id"> & { id: string };
